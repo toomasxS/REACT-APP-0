b14c1e66e43af817ac5ad6562c23fe2fbab17bcd
toomasxS <a14786@oficina.pt>
2026-05-21 09:35:28 +0000

    aura

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+//import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx'
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
+//import Header from './Header.jsx'
+//import Footer from './Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header />
     <App></App>
-    <Footer />
   </StrictMode>,
 )
